7d6f91238669345514d4128866f4c798233397ca
NISAL <535964903@qq.com>
2018-06-08 00:31:35 +0800

    初始化项目

---
+++ release/any-console.js
@@ -0,0 +1,38 @@
+(function(l, i, v, e) { v = l.createElement(i); v.async = 1; v.src = '//' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; e = l.getElementsByTagName(i)[0]; e.parentNode.insertBefore(v, e)})(document, 'script');
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+  typeof define === 'function' && define.amd ? define(factory) :
+  (factory());
+}(this, (function () { 'use strict';
+
+  function styleInject(css, ref) {
+    if ( ref === void 0 ) ref = {};
+    var insertAt = ref.insertAt;
+
+    if (!css || typeof document === 'undefined') { return; }
+
+    var head = document.head || document.getElementsByTagName('head')[0];
+    var style = document.createElement('style');
+    style.type = 'text/css';
+
+    if (insertAt === 'top') {
+      if (head.firstChild) {
+        head.insertBefore(style, head.firstChild);
+      } else {
+        head.appendChild(style);
+      }
+    } else {
+      head.appendChild(style);
+    }
+
+    if (style.styleSheet) {
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+  }
+
+  var css = "";
+  styleInject(css);
+
+})));
---
+++ rollup.config.js
@@ -0,0 +1,56 @@
+import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import postcss from 'rollup-plugin-postcss';
+
+import { writeFileSync } from 'fs';
+
+const isDev = process.env.NODE_ENV === 'development';
+const minify = process.env.MINIFY;
+
+const config = {
+  input: 'src/index.js',
+  output: {
+    file: 'release/any-console.js',
+    format: 'umd',
+    name: 'AnyConsole'
+  },
+  plugins: [
+    postcss({
+      extensions: ['.css', '.scss']
+    })
+  ]
+};
+
+if (isDev) {
+  config.plugins.push(
+    serve({
+      port: 8080,
+      open: true,
+      contentBase: './',
+      historyApiFallback: true,
+      host: '0.0.0.0'
+    }),
+  );
+
+  config.plugins.push(
+    livereload()
+  );
+  
+} else {
+  if (minify) {
+    config.output.file = 'release/any-console.min.js';
+    config.plugins.push(uglify.uglify());
+  } else {
+    config.output.file = 'release/any-console.js';
+  }
+
+  config.plugins = config.plugins.concat([
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ]);
+}
+
+export default config;
