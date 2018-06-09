55d6b26c0c4ca923db553b534c92d99d8f8096d1
NISAL <535964903@qq.com>
2018-06-09 12:34:33 +0800

    完善readme.md

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # any-console
 
-轻量的用于移动设备真机调试的虚拟控制台
+轻量的用于移动设备真机调试的虚拟控制台。
+无需任何修改，无缝接入项目。
 
-Console for mobile devices.
+Console for mobile devices. Without any modification, just need include 'any-console'.
 
 ## 安装 Install
 `npm i any-console -S`
@@ -35,3 +36,10 @@ new ac();
 ## github
 
 https://github.com/hiNISAL/any-console.git
+
+## 计划 TODOS
+
+- [] Network
+- [] Cookie
+- [] LocalStorage
+- [] SessionStorage
--- release/any-console.js
+++ release/any-console.js
@@ -121,7 +121,11 @@
         
       </header>
       <ul class="__any_console-tab-wrapper">
-        <li class="__any_console_tab_item active">Console</l>
+        <li class="__any_console_tab_item active">Console</li>
+        <li class="__any_console_tab_item">Network</li>
+        <li class="__any_console_tab_item">Cookie</li>
+        <li class="__any_console_tab_item">LocalStorage</li>
+        <li class="__any_console_tab_item">SessionStorage</li>
       </ul>
     </div>
 
@@ -134,6 +138,11 @@
           <li>Warn</li>
           <li>Error</li>
         </ul>
+
+      <div class="__any_console-order">
+        <input type="text">
+        <button>确定</button>
+      </div>
       <div class="__any_console-console-panel-log-items">
       
       </div>
